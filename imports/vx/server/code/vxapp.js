d918b6ee87c8083fb72deef36143466f2f8d95c3
sotarules <sotarules@gmail.com>
2021-04-14 10:52:40 -0700

    VX-30 Minor change to VXApp.arrayBufferToBase64 to avoid warning message

--- imports/vx/server/code/vxapp.js
+++ imports/vx/server/code/vxapp.js
@@ -1711,6 +1711,6 @@ VXApp = _.extend(VXApp || {}, {
         for (let i = 0; i < len; i++) {
             binary += String.fromCharCode(bytes[ i ])
         }
-        return new Buffer(binary, "binary").toString("base64")
+        return Buffer.from(binary, "binary").toString("base64")
     }
 })
